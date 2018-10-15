099ad93ea06c9dcaec2f86e2a81b6f0e7d473768
mareksubocz <marsub@wp.pl>
2018-10-16 00:29:29 +0200

    algorytm zachłanny

---
+++ greedy.cpp
@@ -0,0 +1,13 @@
+#define FOR(i,a,b) for(int i=a; i<b; ++i)
+#define ROF(i,a,b) for(int i=a; i>b; --i)
+#define REP(i,n) FOR(i,0,n)
+#define INF 2147483647
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	std::ios_base::sync_with_stdio(false);
+	//std::cin.tie(nullptr);
+	
+	return 0;
+}
