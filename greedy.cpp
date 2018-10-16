7f90233fc186e30930791a5b5b4268105408a8df
mareksubocz <marsub@wp.pl>
2018-10-16 15:51:25 +0200

    wczytywarka plikow z TSPLIB w greedy

--- greedy.cpp
+++ greedy.cpp
@@ -2,12 +2,44 @@
 #define ROF(i,a,b) for(int i=a; i>b; --i)
 #define REP(i,n) FOR(i,0,n)
 #define INF 2147483647
+#define MAX 10000
 #include<bits/stdc++.h>
 using namespace std;
+
+struct point{
+	double x, y;
+};
 
 int main(){
 	std::ios_base::sync_with_stdio(false);
 	//std::cin.tie(nullptr);
-	cout<<"elo"<<endl;
+	point p[MAX];
+	double e[MAX][MAX];
+	ifstream in;
+	in.open("in.txt")
+	int n;
+	int a;
+	string opis, linia;
+	while (!in.eof()){
+		getline(in, opis);
+		getline(in, linia); opis += linia;
+		getline(in, linia);
+		in >> linia; cin >> n;
+		getline(in, linia);
+		getline(in, linia);
+		//wczytalem wstep
+		for (int i = 0; i < n; ++i){
+			cin>>a>>a;
+			p[i].x = a;
+			cin>>a;
+			p[i].y = a;
+		}
+		for (int i = 0; i < n; ++i){
+			for (int j = 0; j < n; ++j){
+				e[i][j] = pow((p[i].x - p[j].x),2)
+			}
+		}
+
+	}
 	return 0;
 }
