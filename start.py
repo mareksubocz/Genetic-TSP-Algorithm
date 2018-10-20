93b1d1df30b07fbc05379dfa36358f7519a438d4
mareksubocz <marsub@wp.pl>
2018-10-20 13:01:13 +0200

    :)

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ from termcolor import colored
 
 for filepath in glob.iglob('data/*.tsp'):
     
-    print(colored("-" + filepath + ":", 'green'))
+    print(colored(filepath + ":", 'green'))
     filepath2 = filepath[0:-3]
     filepath2 = filepath2 + "opt.tour"
     if not glob.glob(filepath2):
