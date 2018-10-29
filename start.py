1e8d252765e8ef7b9e2910967ffb8c180b54d571
mareksubocz <marsub@wp.pl>
2018-10-29 22:43:08 +0100

    dodanie generatedData

--- start.py
+++ start.py
@@ -1,9 +1,16 @@
 import glob
-import os
+import os, re
 from termcolor import colored
 os.system("g++-7 greedy.cpp")
 
-for filepath in glob.iglob('data/*.tsp'):
+#generatedData
+arr = glob.iglob('generatedData/*.tsp')
+arr = sorted(arr, key = lambda x: int(x.split("/")[-1].replace("test","").replace(".tsp","")))
+# arr = sorted(arr, key = lambda x: int(re.search('test(.*).tsp', x).group(1)) )
+#tsplibData
+# arr = sorted(glob.iglob('data/*.tsp'))
+
+for filepath in arr:
     print(colored(filepath + ":", 'green'))
     filepath2 = filepath[0:-3]
     filepath2 = filepath2 + "opt.tour"
